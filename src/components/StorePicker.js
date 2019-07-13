8b2d8a1388d2d53755af916493fb463eabac369f
Michaela Davis <michaela.delight@gmail.com>
2019-07-13 14:59:27 -0700

    end of Events, Refs and this Binding 11/29

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,16 +2,25 @@ import React from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
-  handleClick() {
-    alert('Heyyyy!');
+  myInput = React.createRef();
+
+  // `goToStore` is a property called "goToStore" just like `myInput` is a property on the component that we set to an arrow function which will allow us to bind the value of this to the store component
+  goToStore = (event) => {
+    // 1. Stop the form from submitting
+    event.preventDefault();
+    // 2. Get the text from that input
+    console.log(this);
+    // 3. Change the page to /store/whatever-they-entered
+    
   }
+
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={ this.goToStore }>
         <h2>Please Enter a Store</h2>
-        <button onClick={ this.handleClick }>Click Me</button>
         <input 
           type='text' 
+          ref={ this.myInput }
           required placeholder='Store Name' 
           defaultValue={ getFunName() }>
         </input>
