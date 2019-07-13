4f2d9fb0c5c400c918a329fd85cfac5681025360
Michaela Davis <michaela.delight@gmail.com>
2019-07-13 12:15:07 -0700

    onClick inline event handler defined in component

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,11 +2,19 @@ import React from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+  handleClick() {
+    alert('Heyyyy!');
+  }
   render() {
     return (
       <form className="store-selector">
         <h2>Please Enter a Store</h2>
-        <input type='text' required placeholder='Store Name' defaultValue={ getFunName() }></input>
+        <button onClick={ this.handleClick }>Click Me</button>
+        <input 
+          type='text' 
+          required placeholder='Store Name' 
+          defaultValue={ getFunName() }>
+        </input>
         <button type='submit'>Visit Store →</button>
       </form>
     )
